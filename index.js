6f6914e59478e3dc5252028d132a5fe37c742563
wardha-ian1993 <favianwardhana1993@gmail.com>
2024-05-05 15:26:09 +0700

    add: mongoose

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongodb');
+
+const app = express();
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
+app.use(cors());
+app.use(express.static('public'));
+app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html') });
 
+app.use(bodyParser.urlencoded( { extended: false } ));
+app.use(bodyParser.json());
+
+app.post();
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
